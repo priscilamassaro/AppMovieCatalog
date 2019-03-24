3461c355ed5fb82f281bfe4732088c3637b1f6ba
priscilamassaro <priscila.massaro.job@gmail.com>
2019-03-24 20:46:09 -0300

    app structure, adding fragments

---
+++ app/src/main/java/com/example/appmoviecatalog/adapter/ListMoviesAdapter.kt
@@ -0,0 +1,8 @@
+package com.example.appmoviecatalog.adapter
+
+import android.content.Context
+import com.example.appmoviecatalog.model.Movie
+
+class ListMoviesAdapter(listMovies: List<Movie>, context: Context?) {
+
+}
